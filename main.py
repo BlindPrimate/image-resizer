83ce0102842765762c9b49757d961656ac529081
ecleek <eric_cleek@steris.com>
2021-01-05 15:56:43 -0500

    command line argument parsing added

--- main.py
+++ main.py
@@ -11,22 +11,37 @@ parser.add_argument('--size')
 
 args = parser.parse_args()
 
-path = os.path.join('C:\\Users\\ecleek\\temp')
-size = 700, 700
+default_source = os.path.dirname(os.path.abspath(__file__))
+default_destination = os.path.join(default_source, 'resized')
 
-def resize():
-    output_path = os.path.join(path, "resized")
-    for item in os.listdir(path):
-        if not os.path.isdir(output_path):
-            os.mkdir(output_path)
-        file_path = os.path.join(path, item)
-        if os.path.isfile(file_path):
-            im = Image.open(file_path)
+
+class Resizer:
+    def __init__(self, source=default_source, destination=None, size=700):
+        print(default_source)
+        if source and not destination:
+            self.destination = os.path.join(source, 'resized')
+        else:
+            self.destination = destination
+        self.source = source
+        self.size = (size, size)
+
+    def resize(self):
+        count = 1
+        for item in os.listdir(self.source):
             f, e = os.path.splitext(item)
-            im.thumbnail(size, Image.ANTIALIAS)
-            im.save(os.path.join(output_path, f + '-resized.jpg'), 'JPEG', quality=90)
-            print("saving")
+            if e not in ['.jpg', '.png']:
+                continue
+            if not os.path.isdir(self.destination):
+                os.mkdir(self.destination)
+            file_path = os.path.join(self.source, item)
+            if os.path.isfile(file_path):
+                im = Image.open(file_path)
+                im.thumbnail(self.size, Image.ANTIALIAS)
+                im.save(os.path.join(self.destination, f + '-resized.jpg'), 'JPEG', quality=90)
+                count += 1
+        print("{} images resized and saved to {}".format(count, self.destination))
 
 
 if __name__ == '__main__':
-    resize()
+    resizer = Resizer(source=args.source, destination=args.destination, size=int(args.size))
+    resizer.resize()
